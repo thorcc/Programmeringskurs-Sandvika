5c841b087f7658aa8cb95560a235a5088fe2ce9e
thorcc <thorchrcow@gmail.com>
2021-11-17 12:07:15 +0100

    la til eksempel

--- plott-av-datasett.py
+++ plott-av-datasett.py
@@ -1,20 +1,21 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from pylab import *
 
-f = open('datasett/kolsaastoppen_med_Eirik.csv', 'r')
-lines = f.readlines()[1:] # må droppe den første linjen
-t = [float(line.split(",")[0]) for line in lines]
-x = [float(line.split(",")[1]) for line in lines] 
-y = [float(line.split(",")[2]) for line in lines]
-h = [float(line.split(",")[3]) for line in lines]
+fil = open('datasett/kolsaastoppen_med_Eirik.csv')
+linjer = fil.readlines() # lager en liste med linjene i fil
 
-fig, axs = plt.subplots(2)
-fig.suptitle('Kolsåstoppen med Eirik')
+t = []
+x = []
+y = []
+h = []
 
-axs[0].plot(x, y)
-axs[0].set_title("Koordinater")
+for linje in linjer[1:]: #[1:] dropper den første linjen i filen
+    oppdelt_linje = linje.split(",")
+    t.append(float(oppdelt_linje[0]))
+    x.append(float(oppdelt_linje[1]))
+    y.append(float(oppdelt_linje[2]))
+    h.append(float(oppdelt_linje[3]))
 
-axs[1].plot(t, h)
-axs[1].set_title("Høyde")
-axs[1].set(xlabel="Tid (s)", ylabel="Høyde (moh)")
-
-plt.show()
+title('Kolsåstoppen med Eirik')
+plot(x, y)
+show()
